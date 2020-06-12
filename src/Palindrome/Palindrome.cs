f1f92b8038cc891b3640ca0fd44c453141d9ac8a
jaredsholmes <46760807+jaredsholmes@users.noreply.github.com>
2020-06-11 22:10:17 -0600

    Update Palindrome.cs

--- src/Palindrome/Palindrome.cs
+++ src/Palindrome/Palindrome.cs
@@ -14,7 +14,60 @@
         /// <returns>If the input is a valid palindrome.</returns>
         public bool IsValid(string input)
         {
-            return false;
+            if (input is null)
+            {
+                return false;
+            }
+
+            // Index's needed to compare letters or digits
+            var index1 = 0;
+            var index2 = input.Length - 1;
+
+            while (true)
+            {
+                if (index1 > index2)
+                {
+                    return true;
+                }
+
+                // To hold characters
+                var c1 = input[index1];
+                var c2 = input[index2];
+
+                // Loop forward while we don't have a letter or digit
+                while (!char.IsLetterOrDigit(c1))
+                {
+                    index1++;
+
+                    if (index1 > index2)
+                    {
+                        return true;
+                    }
+
+                    c1 = input[index1];
+                }
+
+                // Loop backward while we don't have a letter or digit
+                while (!char.IsLetterOrDigit(c2))
+                {
+                    index2--;
+
+                    if (index1 > index2)
+                    {
+                        return true;
+                    }
+
+                    c2 = input[index2];
+                }
+
+                if (char.ToLower(c1) != char.ToLower(c2))
+                {
+                    return false;
+                }
+
+                index1++;
+                index2--;
+            }
         }
 
         #endregion
